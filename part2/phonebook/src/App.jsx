910cf9be305800676c0a09e988708f574cbbffa7
Noel Alfonso Obando Martinez <147889470+DigitalGenius27@users.noreply.github.com>
2026-09-11 17:34:54 -0600

    Complete exercise 2.14

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -22,9 +22,11 @@ const PersonForm = ({
       <div>
         name: <input value={newName} onChange={onNameChange} />
       </div>
+
       <div>
         number: <input value={newNumber} onChange={onNumberChange} />
       </div>
+
       <div>
         <button type="submit">add</button>
       </div>
